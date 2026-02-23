e3c74abb31ab1e154c0a62d843a5f6950f127c61
MajoLeon06 <majoleonpaz1321@gmail.com>
2026-02-22 21:22:31 -0600

    Prueba con if

--- Ejercicio4/Program.cs
+++ Ejercicio4/Program.cs
@@ -7,5 +7,30 @@ int activo = int.Parse(Console.ReadLine());
 Console.WriteLine("DATOS COMPROMETIDOS\n1:Ninguno\n2:Internos\n3:Personales\n4:Financieros");
 Console.WriteLine("Ingrese los datos comprometidos:");
 int datos = int.Parse(Console.ReadLine());
-
-
+Console.WriteLine("¿Persistencia detectada? 1:Si 2:No");
+int persistencia = int.Parse(Console.ReadLine());
+Console.WriteLine("Número de usuarios afectados:");
+int users = int.Parse(Console.ReadLine());
+if (incidente>=1 && incidente<=4 && activo>=1 && activo<=4 && datos>=1 && datos<=4 && persistencia>=1 && persistencia<=2)
+{
+    if (incidente==1 && activo==1 && datos==1 && persistencia==2)
+    {
+        Console.WriteLine("Severidad baja");
+    }
+    else if (incidente == 1 && activo == 2 && datos == 1 && persistencia == 2)
+    {
+        Console.WriteLine("Severidad media");
+    }
+    else if (incidente == 1 && activo == 3 && datos == 1 && persistencia == 2)
+    {
+        Console.WriteLine("Severidad media");
+    }
+    else if (incidente == 1 && activo == 4 && datos == 1 && persistencia == 2)
+    {
+        Console.WriteLine("Severidad media");
+    }
+    else if (persistencia==1)
+    {
+        Console.WriteLine("Severidad alta");
+    }
+}
